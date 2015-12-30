4ee225942470559da7a2910761247d8ec86a9492
Ojas Deshpande <ojas25@gmail.com>
2015-12-29 22:25:38 -0500

    Egen Project Added

---
+++ EgenProject_Ojas/src/example/HelloWorld.java
@@ -0,0 +1,10 @@
+package example;/**
+ * Created by Ojas on 12/28/2015.
+ */
+public class HelloWorld {
+  public String sayHelloWorldFrom(String from) {
+    String result = "Hello, world, from " + from;
+    System.out.println(result);
+    return result;
+  }
+}
